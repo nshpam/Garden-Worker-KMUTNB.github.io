3a8246228261794ae1c86376e8101a5c744b1688
Atomute <73423300+Atomute@users.noreply.github.com>
2022-11-24 20:07:00 +0700

    signal app V3

--- Arduino/moisture/moisture.ino
+++ Arduino/moisture/moisture.ino
@@ -1,5 +1,5 @@
-const int AirValue = 3300;   //you need to replace this value with Value_1
-const int WaterValue = 0;  //you need to replace this value with Value_2
+const int AirValue = 3300;   //moiture value from sensor when it is in the air
+const int WaterValue = 0;  //moiture value from sensor when it is in the water
 int soilMoistureValue = 0;
 int soilmoisturepercent=0;
 
@@ -7,11 +7,19 @@ void setup() {
   pinMode(14,OUTPUT);
   Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
   
-  
+
 }
 void loop() {
 soilMoistureValue = analogRead(34);  //put Sensor insert into soil
 Serial.println(soilMoistureValue);
-
-  delay(1000);
+soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+if(soilmoisturepercent < 40)
+{
+  digitalWrite(14,HIGH);
+}
+else if(soilmoisturepercent > 50)
+{
+  digitalWrite(14,LOW);
+}
+  delay(1000);          //delay 1 sec
 }
